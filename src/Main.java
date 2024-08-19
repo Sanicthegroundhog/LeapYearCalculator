ccf4ef542b261c95dd1e5e75ad863c36010ea968
Jacob Atterson <atterjac000@warren.k12.in.us>
2024-08-19 13:37:20 -0400

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,38 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(isLeapYear(-1600));
+        System.out.println(isLeapYear(1600));
+        System.out.println(isLeapYear(2017));
+        System.out.println(isLeapYear(2000));
+    }
+
+    private static boolean isLeapYear(int year) {
+        return div4Check(year);
+    }
+
+    private static boolean div4Check(int year) {
+        if(year % 4 == 0){
+            return div100Check(year);
+        }
+        else{
+            return false;
+        }
+    }
+
+    private static boolean div100Check(int year) {
+        if(year % 100 == 0){
+            return div400Check(year);
+
+        }else{
+            return true;
+        }
+    }
+
+    private static boolean div400Check(int year) {
+        if(year % 400 == 0){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
